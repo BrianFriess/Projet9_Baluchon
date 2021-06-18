c36c1a1811868de0c7c454cecb6074f67e67abb1
Brian Friess <brian.friesspro@gmail.com>
2021-06-18 13:20:52 +0200

    ajout amelioration money et weather

---
+++ BaluchonTests/FakeResponseDataWeather.swift
@@ -0,0 +1,8 @@
+//
+//  FakeResponseDataWeather.swift
+//  BaluchonTests
+//
+//  Created by Brian Friess on 14/06/2021.
+//
+
+import Foundation
---
+++ BaluchonTests/URLSessionFake.swift
@@ -0,0 +1,8 @@
+//
+//  URLSessionFake.swift
+//  BaluchonTests
+//
+//  Created by Brian Friess on 15/06/2021.
+//
+
+import Foundation
